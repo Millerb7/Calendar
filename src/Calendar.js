1d87a224b4b88086ef17599e1422449df2f608ed
Miller <Millerjboyd@gmail.com>
2022-03-08 14:55:54 -0600

    entries are now created based of days in month from date object

--- src/Calendar.js
+++ src/Calendar.js
@@ -15,27 +15,14 @@ class entry {
   }
 }
 
+// creates an entry for every day in a month
 const gatherEntries = (date) => {
   const entries = [];
   // supposed to query to db for all necessary incidents based on date 
 
   // create en entry object for every day in a month
-  for(var i = 0; i < 28; i++) {
-    const temp = new entry(i,date,'bobby does hobby' + i);
-    entries.push(temp);
-  }
-  // add the rest of the days for everyone outside of feb.
-  if(date.getMonth() === 0 || date.getMonth() === 2 || date.getMonth() === 4 || date.getMonth() === 6 || date.getMonth() === 7 || date.getMonth() === 9 || date.getMonth() === 11) {
-    let temp = new entry(29,date,'bobby does hobby' + 29);
-    entries.push(temp);
-    temp = new entry(30,date,'bobby does hobby' + 30);
-    entries.push(temp);
-    temp = new entry(31,date,'bobby does hobby' + 31);
-    entries.push(temp);
-  } else if(date.getMonth() !== 2) {
-    let temp = new entry(29,date,'bobby does hobby' + 29);
-    entries.push(temp);
-    temp = new entry(30,date,'bobby does hobby' + 30);
+  for(var i = 0; i < ( new Date(date.getFullYear(), date.getMonth()+1, 0).getDate() ); i++) {
+    const temp = new entry(i+1,date,'bobby does hobby ' + i);
     entries.push(temp);
   }
   // entries now holds objects for every day to be displayed
@@ -46,11 +33,11 @@ const gatherEntries = (date) => {
 function Calendar(props) {
   return (
       <div id="calendar">
-        <div id="interval_selection">
+        <nav id="interval_selection">
           <button id="decrement_time" className="change_interval" onClick={() => {console.log('decrement');}}>(</button>
           <h3>{months[props.date.getMonth()]}, {props.date.getFullYear()}</h3>
           <button id="decrement_time" className="change_interval" onClick={() => {console.log('increment');}}>)</button>
-        </div>
+        </nav>
         <div id="interval_display">
             {gatherEntries(props.date).map((entry) => (
             <div>
